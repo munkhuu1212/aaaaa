6dafd3b09deb28af8eda546c05e136244145e87d
munkhuu1212 <132901264+munkhuu1212@users.noreply.github.com>
2025-02-12 23:26:16 +0800

    Create bot.py
    addd

---
+++ bot.py
@@ -0,0 +1,21 @@
+class AmortizedStack:
+    deawdf __iniasdt__(self, k):  
+        # Стек хадгsadawdaалах жагсаалт
+        selwdfad.stack = []
+        #adaww "bwaackup" үйлдлийг хийх зай буюу k утга
+        awdadwself.awdawdk = kasdaw
+        # wsh(sadelf, value):  
+        # Стекaw рүү шиapend(value)
+        # a len(ad
+    dawdef pod Хэрэв стек d
+    dadwef badackupad(self):  
+        # Нөөцлөлт хийw
+    dawdef total_cawost(self):  
+        # Нwdaийт зардал: af.stack) + self.backups
+
+# Жишээ dk)  # Шинэ стек үүсгэх
+d
+# 1-ээс 20 хүрw1, operations + 1):
+    stack.push(i)  # push үйлдэл
+    iawdf i % 3 == 0:  
+        stack.pdаах w хорогдуулсан зардал: {stack.total_cost() / operations:.2f}")da
